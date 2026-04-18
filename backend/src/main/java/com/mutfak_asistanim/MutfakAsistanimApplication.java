4f95f02179d2865c7befd115425fe626395d420d
ugurnusretoglu <ugurnusretoglu2004@gmail.com>
2026-04-18 15:14:26 +0300

    Initialize Spring Boot backend

---
+++ backend/src/main/java/com/mutfak_asistanim/MutfakAsistanimApplication.java
@@ -0,0 +1,13 @@
+package com.mutfak_asistanim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutfakAsistanimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutfakAsistanimApplication.class, args);
+	}
+
+}
