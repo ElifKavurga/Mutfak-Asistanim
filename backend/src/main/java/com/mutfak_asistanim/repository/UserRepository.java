33c71b2c772e4507f7c8194095b2870143e41fc8
ugurnusretoglu <ugurnusretoglu2004@gmail.com>
2026-04-25 16:48:56 +0300

    feat: implement JWT authentication and Spring Security configuration

---
+++ backend/src/main/java/com/mutfak_asistanim/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.mutfak_asistanim.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mutfak_asistanim.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	Optional<User> findByUsername(String username);
+	
+}
